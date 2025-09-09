b7c73c9809404dc1c5e35be43f514d1b4603e9ee
RealElvince <obuongely@yahoo.com>
2025-09-09 19:22:50 +0300

    rename sales schema to order schema

--- schemas/sales_schema.py
+++ schemas/sales_schema.py
@@ -1,6 +1,6 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, DateType
 
-sales_schema = StructType([
+order_schema = StructType([
     StructField("product_id", IntegerType(), True),
     StructField("customer_id", StringType(), True),
     StructField("order_date", DateType(), True),
