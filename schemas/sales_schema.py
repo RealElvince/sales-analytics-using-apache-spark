c469168a610f2ba18e34193a43a03d30790e8cc9
RealElvince <obuongely@yahoo.com>
2025-09-09 19:19:56 +0300

    define schema for sales

---
+++ schemas/sales_schema.py
@@ -0,0 +1,9 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, DateType
+
+sales_schema = StructType([
+    StructField("product_id", IntegerType(), True),
+    StructField("customer_id", StringType(), True),
+    StructField("order_date", DateType(), True),
+    StructField("location", StringType(), True),
+    StructField("source_order", StringType(), True),
+])
