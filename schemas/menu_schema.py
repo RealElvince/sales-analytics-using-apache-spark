2c3fb530ef975e018cfc0e182a23c1022a4c73c7
RealElvince <obuongely@yahoo.com>
2025-09-10 19:11:35 +0300

    change schema name

--- schemas/menu_schema.py
+++ schemas/menu_schema.py
@@ -1,5 +1,5 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
-menu_schema = StructType([
+order_menu_schema = StructType([
     StructField("product_id", IntegerType(), True),
     StructField("product_name", StringType(), True)])
